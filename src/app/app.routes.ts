c6953dac913bc8218f3f800335b9a998d113998d
kimiya <samadikimiya30.gmail.com>
2024-12-11 18:31:10 +0330

    add recent transaction page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './authentication/login/login.component';
 import {SidebarComponent } from './shared/sidebar/sidebar.component';
 import {OverviewComponent} from './dashboard/overview/overview.component';
 import {BalancesComponent} from './dashboard/balances/balances.component';
+import {TransactionsComponent} from './dashboard/transactions/transactions.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'sidebar', component: SidebarComponent},
   { path: 'overview', component: OverviewComponent},
   { path: 'balances', component: BalancesComponent},
+  { path: 'transactions', component: TransactionsComponent},
 ];
--- src/app/dashboard/transactions/transactions.component.ts
+++ src/app/dashboard/transactions/transactions.component.ts
@@ -1,12 +1,102 @@
 import { Component } from '@angular/core';
+import {SidebarComponent} from '../../shared/sidebar/sidebar.component';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell, MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {MatIcon} from '@angular/material/icon';
+import {MatButton} from '@angular/material/button';
+import {DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [],
+  imports: [
+    SidebarComponent,
+    MatCard,
+    MatCardTitle,
+    MatCardHeader,
+    MatCardContent,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatIcon,
+    MatRow,
+    MatHeaderRow,
+    MatButton,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatCellDef,
+    MatHeaderCellDef,
+    DecimalPipe
+  ],
   templateUrl: './transactions.component.html',
-  styleUrl: './transactions.component.scss'
+  styleUrls: ['./transactions.component.scss']
 })
 export class TransactionsComponent {
-
+  transactions = [
+    {
+      icon: 'toys',
+      item: 'GTR 5',
+      shop: 'Gadget & Gear',
+      date: '17 May, 2023',
+      method: 'Credit Card',
+      amount: 160.0,
+    },
+    {
+      icon: 'checkroom',
+      item: 'Polo Shirt',
+      shop: 'XL fashions',
+      date: '17 May, 2023',
+      method: 'Credit Card',
+      amount: 20.0,
+    },
+    {
+      icon: 'restaurant',
+      item: 'Biriyani',
+      shop: 'Hajir Biriyani',
+      date: '17 May, 2023',
+      method: 'Credit Card',
+      amount: 12.0,
+    },
+    {
+      icon: 'local_movies',
+      item: 'Movie Ticket',
+      shop: 'Inox',
+      date: '17 May, 2023',
+      method: 'Credit Card',
+      amount: 15.0,
+    },
+    {
+      icon: 'local_taxi',
+      item: 'Taxi Fare',
+      shop: 'Uber',
+      date: '17 May, 2023',
+      method: 'Credit Card',
+      amount: 10.0,
+    },
+    {
+      icon: 'local_pizza',
+      item: 'Pizza',
+      shop: 'Pizza Hut',
+      date: '17 May, 2023',
+      method: 'Credit Card',
+      amount: 20.0,
+    },
+    {
+      icon: 'keyboard',
+      item: 'Keyboard',
+      shop: 'Gadget & Gear',
+      date: '17 May, 2023',
+      method: 'Credit Card',
+      amount: 30.0,
+    },
+  ];
 }
